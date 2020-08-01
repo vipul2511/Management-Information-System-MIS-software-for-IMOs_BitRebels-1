31cba27fa7c01d821d44d687003dfcf1ae3e8608
vipul2511 <vshrimali709@gmail.com>
2020-08-02 01:27:27 +0530

    Updated

--- server.js
+++ server.js
@@ -16,37 +16,38 @@ const client = require('twilio')(
 );
 const twilioNumber = '+12029725135'
 cron.schedule("* * * * *", function() {
-  // let key;
-  // let Data=[];
-  // admin.database().ref('users/').child('EMI/').orderByKey().on('child_added',function(snapshot){
-  //   Data.push(snapshot.key);
-  // });
-  //   const now = moment();
-  //   const dateFormatted = now.format('DDMMYYYY');
-  //   Data.forEach(item=>{
-  //   admin.database().ref('users/').child('EMI/'+item).limitToFirst(1).on('child_added',snapshot => {
-  //       let date=snapshot.val().scheduled_payment;
-  //       let phone=snapshot.val().phone;
-  //       let amount=snapshot.val().amount;
-  //       key=snapshot.key;
-  //       console.log(`the date is ${date}`);
-  //       console.log(`the data item ${Data}`);
-  //       console.log(snapshot.val())
-  //       if(date==dateFormatted){
-  //           client.messages
-  //     .create({
-  //       from: process.env.TWILIO_PHONE_NUMBER,
-  //       to: phone,
-  //       body:`Your Loan EMI is ${amount} to be paid at ${date}. Kindly Ignore this message paid already.`
-  //     })
-  //       .then(message => console.log(message,'success'))
-  //           .catch(err => console.log(err));
-  //       }
-  //   });
-  // });
+  let key;
+  let Data=[];
+  admin.database().ref('users/').child('EMI/').orderByKey().on('child_added',function(snapshot){
+    Data.push(snapshot.key);
+  });
+    const now = moment();
+    const dateFormatted = now.format('DD/M/YYYY');
+    console.log(dateFormatted);
+    Data.forEach(item=>{
+    admin.database().ref('users/').child('EMI/'+item).limitToFirst(1).on('child_added',snapshot => {
+        let amount=snapshot.val().scheduled_payment;
+        let phone=snapshot.val().phone;
+        let date=snapshot.val().payment_date;
+        key=snapshot.key;
+        console.log(`the date is ${date}`);
+        console.log(`the data item ${Data}`);
+        console.log(snapshot.val())
+        if(date==dateFormatted){
+            client.messages
+      .create({
+        from: process.env.TWILIO_PHONE_NUMBER,
+        to: '+91'+phone,
+        body:`Your Loan EMI is ${amount} to be paid at ${date}. Kindly Ignore this message paid already.`
+      })
+        .then(message => console.log(message,'success'))
+            .catch(err => console.log(err));
+        }
+    });
+  });
     
     //  admin.database().ref('users/EMIDates/date').child(key).remove();
-    console.log("running all time");
+    console.log("running all the time");
   });
 // Initalize middleware
 app.use(express.json({ extended: false }));
