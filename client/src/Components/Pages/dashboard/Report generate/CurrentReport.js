e5bee6c2b77d78b0679ac1d0a2d44fddec461267
vipul2511 <vshrimali709@gmail.com>
2020-08-03 16:35:27 +0530

    added the report generation process

--- client/src/Components/Pages/dashboard/Report generate/CurrentReport.js
+++ client/src/Components/Pages/dashboard/Report generate/CurrentReport.js
@@ -11,7 +11,6 @@ class MonthlyReport extends Component {
     super(props);
    this.state={
       userData:[],
-      userItem:[]
     }
   }
   componentDidMount(){
@@ -70,46 +69,6 @@ class MonthlyReport extends Component {
         </tr>
           );
     })}
-    <tr  style={{width:'100px',height:'100px'}}>
-          <td>47512481</td>
-          <td>Varshang Shrimali</td>
-          <td>Rajasthan</td>
-          <td>568952978158</td>
-          <td>BISPS3073A</td>
-          <td>555555555555</td>
-          <td>784596123</td>
-          <td style={{color:'green'}}>Approved</td>
-         </tr>
-         <tr style={{width:'100px',height:'100px'}}>
-         <td>47512481</td>
-          <td>Bharat shrotriya</td>
-          <td>Rajasthan</td>
-          <td>595432485949	PISBN5541S</td>
-          <td>PISBN5541S</td>
-          <td>68768768768733</td>
-          <td>3333</td>
-          <td style={{color:'green'}}>Approved</td>
-         </tr>
-         <tr style={{width:'100px',height:'100px'}}>
-         <td>227264758</td>
-          <td>	Varshang Shrimali</td>
-          <td>Tamil Nadu</td>
-          <td>PISBN2214S</td>
-          <td>PISBN5541S</td>
-          <td>	224512111</td>
-          <td>250000</td>
-          <td style={{color:'green'}}>Approved</td>
-         </tr>
-         <tr style={{width:'100px',height:'100px'}}>
-         <td>826312030			254210000	250000</td>
-          <td>	Varshang Shrimali</td>
-          <td>Rajasthan</td>
-          <td>PISBN2214S</td>
-          <td>	269191509702</td>
-          <td>	224512111</td>
-          <td>250000</td>
-          <td style={{color:'Yellow'}}>Pending</td>
-         </tr>
         </tbody>
       </Table>
       </div>
